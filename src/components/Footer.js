42ef1c31b9ea482325b7b5cc596654ec80d41851
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-09-05 21:31:37 +0400

    added semantic components

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <img src=''/>
+      <h4>Doormat Navigation</h4>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Menu</a></li>
+        <li><a href='#'>Reservations</a></li>
+        <li><a href='#'>Order Online</a></li>
+        <li><a href='#'>Login</a></li>
+      </ul>
+      <h4>Contact</h4>
+      <ul>
+        <li><a href='#'>Address</a></li>
+        <li><a href='#'>phone number</a></li>
+        <li><a href='#'>email</a></li>
+      </ul>
+      <h4>Social Media Links</h4>
+      <ul>
+        <li><a href='#'>Address</a></li>
+        <li><a href='#'>phone number</a></li>
+        <li><a href='#'>email</a></li>
+      </ul>
+    </footer>
+  )
+}
