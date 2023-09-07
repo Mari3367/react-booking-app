2e4534d745519166f9bfb52108298121bdaa070a
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-09-08 00:41:13 +0400

    added testimonials section with basic styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Highlights from './Highlights';
+import Testimonials from './Testimonials';
 
 export default function Main() {
   return (
     <main>
       <Highlights />
+      <Testimonials />
     </main>
   )
 }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {BiSolidStar} from 'react-icons/bi';
+import {BiSolidStarHalf} from 'react-icons/bi';
+import customer1 from '../assets/maria';
+import customer2 from '../assets/antony';
+import customer3 from '../assets/tamika';
+import customer4 from '../assets/brandon';
+
+
+const customers = [
+    {
+        name: 'Maria Sanchez',
+        image: customer1,
+        rate: 4.5,
+        review: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."'
+    },
+    {
+        name: 'Antony Clifton',
+        image: customer2,
+        rate: 5,
+        review: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."'
+    },
+    {
+        name: 'Tamika Jackson',
+        image: customer3,
+        rate: 4.5,
+        review: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."'
+    },
+    {
+        name: 'Brandon Ming',
+        image: customer4,
+        rate: 4,
+        review: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."'
+    }
+]
+
+
+export default function Testimonials() {
+  return (
+    <section className='testimonials-box'>
+        <h2>What people say about us!</h2>
+        <div className='people'>
+            {customers.map((customer) => {
+                return <div className='customer'>
+                            <div className='customer-pic-name'>
+                              <img src={customer.image} alt={customer.name} className='customer-photo'/>
+                              <h5>{customer.name}</h5>
+                            </div>
+                            {customer.rate === 5 ? <div className='rate-stars'><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /></div> : customer.rate === 4.5 ? <div className='rate-stars'><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStarHalf /></div> : <div className='rate-stars'><BiSolidStar /><BiSolidStar /><BiSolidStar /><BiSolidStar /></div>}
+                            <p className='review-text'>{customer.review}</p>
+                        </div>
+            })}
+        </div>
+    </section>
+  )
+}
