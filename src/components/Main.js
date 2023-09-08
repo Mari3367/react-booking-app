0a7dcbb03e2f3a51cd772199c8bc9f95dc834af7
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-09-08 22:14:55 +0400

    added story section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Highlights from './Highlights';
 import Testimonials from './Testimonials';
+import Story from './Story';
 
 export default function Main() {
   return (
     <main>
       <Highlights />
       <Testimonials />
+      <Story />
     </main>
   )
 }
